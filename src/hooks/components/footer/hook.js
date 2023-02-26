045f653a63dd10153462d839be993c2f6895ed06
george <hyunrim03@naver.com>
2023-02-27 00:22:29 +0900

    commit

---
+++ src/hooks/components/footer/hook.js
@@ -0,0 +1,15 @@
+function useFooterDetail() {
+  const data = [
+    {
+      name: "LINKTREE",
+      url: "https://linktr.ee/devrent",
+    },
+    {
+      name: "INSTGRAM",
+      url: "https://instagram.com/devrent_official",
+    }
+  ]
+  return { data };
+}
+
+export default useFooterDetail;
