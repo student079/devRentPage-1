a74151f7010c91e76f0e0bec66fe1f9eda5f4624
logoo03 <althj03@gmail.com>
2023-02-27 01:19:54 +0900

    대신 push

--- src/container/home/vision.jsx
+++ src/container/home/vision.jsx
@@ -11,7 +11,7 @@ const Vision = () => {
             className=" md:w-[720px] sm:w-[349px] w-[1000px] h-auto mb-[50px] sm:mb-[100px] flex flex-row md:flex-col sm:flex-col justify-between md:items-center sm:items-center"
             key={index}
           >
-            <span className=" text-center w-[200px] text-[40px] sm:text-[30px] font-semibold mr-[20px] sm:mr-0 md:mr-0">
+            <span className=" text-center w-auto text-[40px] sm:text-[30px] font-semibold mr-[20px] sm:mr-0 md:mr-0">
               {data.visionTitle}
             </span>
             <hr className=" w-[1px] h-auto bg-black" />
